f34821ea5aad11971b51fc89772ac971b1f4360c
rto07 <tar845@Rauls-MBP.attlocal.net>
2018-05-03 14:12:17 -0500

    Checkpoint 27 Loops

---
+++ loops.js
@@ -0,0 +1,18 @@
+var shoppingCart = [
+  {
+      id: 0,
+      name: 'Mens Shirt',
+      price: 20,
+      size: 'Large'
+  },
+  
+  {
+      id: 1,
+      name: 'kids shirt',
+      price: 15,
+      size: 'small'
+  }
+  ]
+for (i = 0; i < shoppingCart.length; i++){
+  console.log(shoppingCart[i].name);
+}
